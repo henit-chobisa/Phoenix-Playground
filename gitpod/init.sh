59ea190be58b1445e95e3597f54387c7ae330ee7
Henit Chobisa <chobisa.henit@gmail.com>
2023-08-15 06:16:05 +0000

    fix: Update gitpod configuration

--- gitpod/init.sh
+++ gitpod/init.sh
@@ -2,6 +2,7 @@
 
 #init db
 psql --command "CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';" 
+psql --command "CREATE ROLE postgres SUPERUSER LOGIN PASSWORD 'postgres';"
 
 #reset app 
 gp env PHX_APP=""
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chat.MixProject do
     [
       app: :chat,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
